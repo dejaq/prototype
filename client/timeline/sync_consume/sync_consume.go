0a1ce0fe4f7d738192c6d4e670c055da0af243ef
B.G.Adrian <adrian@coder.today>
2020-02-16 19:24:08 +0200

    added latency calculator for each msg (#45)

--- client/timeline/sync_consume/sync_consume.go
+++ client/timeline/sync_consume/sync_consume.go
@@ -1,8 +1,13 @@
 package sync_consume
 
 import (
+	"bytes"
 	"context"
+	"strconv"
 	"strings"
+	"time"
+
+	dtime "github.com/dejaq/prototype/common/time"
 
 	"github.com/dejaq/prototype/client/timeline/consumer"
 	"github.com/dejaq/prototype/common/timeline"
@@ -10,39 +15,91 @@ import (
 	"go.uber.org/atomic"
 )
 
+type Average struct {
+	sum int64
+	n   int
+}
+
+func (a Average) Add(x int64) {
+	if x <= 0 {
+		return
+	}
+	a.sum += x
+	a.n++
+}
+func (a Average) Get() int64 {
+	if a.n > 0 {
+		return a.sum / int64(a.n)
+	}
+	return 0
+}
+
 type SyncConsumeConfig struct {
 	Consumer *consumer.Consumer
 }
 
-func Consume(ctx context.Context, msgsCounter *atomic.Int64, conf *SyncConsumeConfig) error {
-	conf.Consumer.Start(ctx, func(lease timeline.Lease) {
-		if lease.GetConsumerID() != conf.Consumer.GetConsumerID() {
-			log.Fatalf("server sent message for another consumer me=%s sent=%s", conf.Consumer.GetConsumerID(), lease.GetConsumerID())
-		}
-		if !strings.Contains(lease.Message.GetID(), conf.Consumer.GetTopicID()) {
-			log.Fatalf("server sent message for another topic: %s sent consumerID: %s, msgID: %s, producedBy: %s",
-				conf.Consumer.GetTopicID(), lease.GetConsumerID(), lease.Message.GetID(), lease.Message.GetProducerGroupID())
-		}
-		//Process the messages
-		msgsCounter.Dec()
-		err := conf.Consumer.Delete(ctx, []timeline.Message{{
-			ID:          lease.Message.ID,
-			TimestampMS: lease.Message.TimestampMS,
-			BucketID:    lease.Message.BucketID,
-			Version:     lease.Message.Version,
-		}})
-		if err != nil {
-			log.Errorf("delete failed", err)
-		}
-		//logrus.Printf("received message id='%s' body='%s' from bucket=%d\n", lease.Message.id, string(lease.Message.Body), lease.Message.BucketID)
-	})
+type syncconsumer struct {
+	avg         *Average
+	ctx         context.Context
+	msgsCounter *atomic.Int64
+	conf        *SyncConsumeConfig
+}
 
-	//log.Info("consumer handshake success")
-
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		}
+func (sc *syncconsumer) callback(lease timeline.Lease) {
+	if lease.GetConsumerID() != sc.conf.Consumer.GetConsumerID() {
+		log.Fatalf("server sent message for another consumer me=%s sent=%s", sc.conf.Consumer.GetConsumerID(), lease.GetConsumerID())
 	}
+	if !strings.Contains(lease.Message.GetID(), sc.conf.Consumer.GetTopicID()) {
+		log.Fatalf("server sent message for another topic: %s sent consumerID: %s, msgID: %s, producedBy: %s",
+			sc.conf.Consumer.GetTopicID(), lease.GetConsumerID(), lease.Message.GetID(), lease.Message.GetProducerGroupID())
+	}
+	//Process the messages
+	err := sc.conf.Consumer.Delete(sc.ctx, []timeline.Message{{
+		ID:          lease.Message.ID,
+		TimestampMS: lease.Message.TimestampMS,
+		BucketID:    lease.Message.BucketID,
+		Version:     lease.Message.Version,
+	}})
+
+	sc.msgsCounter.Dec()
+
+	if err != nil {
+		log.Errorf("delete failed", err)
+		return
+	}
+
+	//retrieve the time when it was created, so we can see how long it took to process it
+	parts := bytes.SplitN(lease.Message.Body[:25], []byte{'|'}, 2)
+	if len(parts) != 2 || len(parts[0]) == 0 {
+		log.Errorf("malformed body (ts for latency not found) id=%s body=%s", lease.Message.ID, lease.Message.Body)
+		return
+	}
+
+	createdMSTs, err := strconv.Atoi(string(parts[0]))
+	if err != nil {
+		log.Errorf("malformed lateny ts, not an int id=%s err=%v", lease.Message.ID, err)
+		return
+	}
+
+	currentMSTS := dtime.TimeToMS(time.Now().UTC())
+	if int64(currentMSTS) <= int64(createdMSTs)-2 {
+		log.Errorf("we invented a time machine id=%s err=%v", lease.Message.ID, err)
+		return
+	}
+	sc.avg.Add(int64(currentMSTS) - int64(createdMSTs))
+}
+
+func Consume(ctx context.Context, msgsCounter *atomic.Int64, conf *SyncConsumeConfig) (int64, error) {
+
+	sc := &syncconsumer{
+		avg:         &Average{},
+		ctx:         ctx,
+		msgsCounter: msgsCounter,
+		conf:        conf,
+	}
+
+	conf.Consumer.Start(ctx, sc.callback)
+
+	<-ctx.Done()
+	return sc.avg.Get(), nil
 }
