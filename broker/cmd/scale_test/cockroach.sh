048d477db19351a324746396751e0f5747f51f25
Adrian BG <adrian@coder.today>
2020-02-16 19:31:50 +0200

    removed .sh

--- broker/cmd/scale_test/cockroach.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cockroach quit --insecure
-cockroach start --insecure --background
-cockroach sql --insecure --execute "CREATE USER IF NOT EXISTS duser;"
-cockroach sql --insecure --execute "CREATE DATABASE dejaq;"
-cockroach sql --insecure --execute "GRANT ALL ON DATABASE dejaq TO duser;"
