a08f21f4ffc2f8dcf47b1bf65397fbae86c17ed3
davidescus <rahthman.s@gmail.com>
2020-04-25 15:35:50 +0300

    remove double quotes from bash files

--- broker/cmd/start_producer.sh
+++ broker/cmd/start_producer.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 go install github.com/dejaq/prototype/broker/cmd/dejaqcli-producer
-TOPIC=largetopic NAME=clitest CONSTANT_TICK_DURATION=1s SINGLE_BURST_EVENTS=20000 dejaqcli-producer
+TOPIC=largetopic NAME=clitest CONSTANT_TICK_DURATION=1s SINGLE_BURST_EVENTS=2000 dejaqcli-producer
 
