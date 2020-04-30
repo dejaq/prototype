14558e4222ba89d637dd29e2d64f269c22a97cad
Adrian BG <adrian@coder.today>
2020-04-30 18:53:11 +0300

    cockroach ID optimized for bucket ranges reads

--- broker/pkg/storage/cockroach/cockroach.go
+++ broker/pkg/storage/cockroach/cockroach.go
@@ -5,14 +5,12 @@ import (
 	"database/sql"
 	"fmt"
 	"strings"
-	"time"
 	"unsafe"
 
 	storageTimeline "github.com/dejaq/prototype/broker/pkg/storage/timeline"
 
 	"github.com/dejaq/prototype/broker/domain"
 	derrors "github.com/dejaq/prototype/common/errors"
-	dtime "github.com/dejaq/prototype/common/time"
 	"github.com/dejaq/prototype/common/timeline"
 	"github.com/lib/pq"
 	"github.com/sirupsen/logrus"
@@ -21,6 +19,9 @@ import (
 var (
 	stmtTopic = `
 CREATE TABLE  IF NOT EXISTS "$TOPIC" (
+	/* we leverage the ORDERED keys in ranges from cockroachDB and sort the items such 
+	to make the LeaseUpdates optimal. bucketIDs are zero padded left to be sorted lexicographically */
+	bucketid_msgid STRING NOT NULL,
 	id STRING NOT NULL,
 	timeline INT NOT NULL,
 	bucket_id INT NOT NULL,    
@@ -29,7 +30,52 @@ CREATE TABLE  IF NOT EXISTS "$TOPIC" (
 	consumer_id STRING,
 	body STRING,
 	version INT NOT NULL,
-	CONSTRAINT "primary" PRIMARY KEY (id ASC),
+	CONSTRAINT "primary" PRIMARY KEY (bucketid_msgid ASC),
+	/* used by GetAndLease , the bucketID is in there */
+	INDEX $TOPIC_index_timeline_bucket (bucketid_msgid, timeline ASC), 
+	/*used by SelectByConsumerID */
+	INDEX $TOPIC_index_consumerID_bucket (consumer_id, bucket_id ASC),   
+	FAMILY stable (bucketid_msgid, id, ts, bucket_id, producer_group_id, body),  
+ 	FAMILY mutable (timeline, consumer_id, version)
+);
+`
+
+	TOIMPLEMENT = `
+CREATE TABLE IF NOT EXISTS "v2_ts_$TOPIC" (
+	/*bucketid_unixmsTS_msgid */
+	tsid STRING NOT NULL,
+	consumer_id STRING,
+	/* used for leases */
+	CONSTRAINT "primary" PRIMARY KEY (tsid ASC),
+	/* used by preloading */
+	INDEX $TOPIC_index_with_cid (tsid, consumer_id ASC),
+	/* used by Hydrate */
+	INDEX $TOPIC_INDEX_consumer_id (consumer_id ASC),
+);
+
+Insert - both tables
+Delete - both tables
+Lease: 
+//get the available messages from my bucket 29, max TS (now) and the maximum msgID (ÿ=255byte largest character)
+//buketID and TS have leading padding zeros
+DELETE FROM FROM v2_ts WHERE tsid < 029_001234433112+1 LIMIT 10 RETURNING ID;
+//move them up to the timeline
+INSERT INTO v2_ts (new tsids .... + consumerID);
+//hope we will hit as few as possible different ranges as they are grouped by bucket_id
+SELECT * FROM v2_meta WHERE bucket_id_msg_id IN ( .... );
+
+
+CREATE TABLE  IF NOT EXISTS "v2_meta_$TOPIC" (
+	bucket_id_msg_id STRING NOT NULL,
+	id STRING NOT NULL,
+	timeline INT NOT NULL,
+	bucket_id INT NOT NULL,    
+	ts INT NOT NULL,
+	producer_group_id STRING NOT NULL,
+	consumer_id STRING,
+	body STRING,
+	version INT NOT NULL,
+	CONSTRAINT "primary" PRIMARY KEY (bucket_id_msg_id ASC),
 	/* used by GetAndLease */
 	INDEX $TOPIC_index_timeline_bucket (timeline, bucket_id ASC),
 	/*used by SelectByConsumerID */
@@ -75,6 +121,13 @@ func min(a, b int) int {
 	return b
 }
 
+func padBucketID(bucketID uint16) string {
+	return fmt.Sprintf("%05d", bucketID)
+}
+func timelineMsgID(bucketID uint16, msgID string) string {
+	return padBucketID(bucketID) + "_" + msgID
+}
+
 func (c *CRClient) Insert(ctx context.Context, req timeline.InsertMessagesRequest) error {
 	var batch []timeline.InsertMessagesRequestItem
 	result := make(derrors.MessageIDTupleList, 0)
@@ -117,6 +170,7 @@ func (c *CRClient) Insert(ctx context.Context, req timeline.InsertMessagesReques
 
 		//https://godoc.org/github.com/lib/pq#hdr-Bulk_imports
 		stmt, err := txn.PrepareContext(ctx, pq.CopyIn(table(req.GetTimelineID()),
+			"bucketid_msgid",
 			"id",
 			"bucket_id",
 			"ts",
@@ -133,12 +187,13 @@ func (c *CRClient) Insert(ctx context.Context, req timeline.InsertMessagesReques
 		var failedSt bool
 		for i := range batch {
 			_, err = stmt.Exec(
+				timelineMsgID(batch[i].BucketID, batch[i].GetID()),
 				batch[i].GetID(),
 				batch[i].BucketID,
 				batch[i].TimestampMS,
 				batch[i].TimestampMS,
 				req.GetProducerGroupID(),
-				batch[i].Body,
+				batch[i].GetBody(),
 				batch[i].Version)
 			if err != nil {
 				failedSt = true
@@ -168,38 +223,90 @@ func (c *CRClient) Insert(ctx context.Context, req timeline.InsertMessagesReques
 	return result
 }
 
+type leaseQuery struct {
+	ctx                context.Context
+	queryUpdate        string
+	consumerId         []byte
+	leaseDurationMS    uint64
+	maxTimestamp       uint64
+	limit              int
+	timelineID         []byte
+	minInclusiveBucket uint16
+	maxExclusiveBucket uint16
+}
+
 func (c *CRClient) GetAndLease(ctx context.Context, timelineID []byte, buckets domain.BucketRange, consumerId []byte, leaseMs uint64, limit int, currentTimeMS uint64, maxTimestamp uint64) ([]timeline.Lease, bool, error) {
+	//OPTIMISTIC we make a query for available messages first , is faster
 
 	//https://www.cockroachlabs.com/docs/v19.2/update.html#update-and-return-values
 	queryUpdate := strings.Replace(`UPDATE "$TOPIC" SET timeline = GREATEST($1, timeline) + $2, consumer_id = $3 WHERE 
-		( timeline <= $1 OR (timeline > $1 AND timeline <= $4 AND consumer_id = NULL )) 
-		AND bucket_id IN ($BUCKETS)
-		LIMIT $5
-		RETURNING id;
+		 bucketid_msgid >= $4 AND bucketid_msgid < $5 AND timeline <= $1 
+		LIMIT $6
+		RETURNING bucketid_msgid;
 	`, "$TOPIC", table(string(timelineID)), -1)
-	queryUpdate = strings.Replace(queryUpdate, "$BUCKETS", buckets.CSV(), 1)
 	//Note: the server will automatically do some retries https://www.cockroachlabs.com/docs/stable/transactions.html#automatic-retries
 
-	st, err := c.db.PrepareContext(ctx, queryUpdate)
+	availableLeases, hasMore, err := c.leaseByQuery(leaseQuery{
+		ctx:             ctx,
+		queryUpdate:     queryUpdate,
+		consumerId:      consumerId,
+		leaseDurationMS: leaseMs,
+		// notice the currentTimeMS !!!! this means only available (from the past TS) will be returned
+		maxTimestamp:       currentTimeMS,
+		limit:              limit,
+		timelineID:         timelineID,
+		minInclusiveBucket: buckets.Min(),
+		maxExclusiveBucket: buckets.Max() + 1,
+	})
+	if err != nil || len(availableLeases) >= limit {
+		//if there were enough available messages we return these
+		return availableLeases, hasMore, err
+	}
+
+	//TODO make this work
+	////there were not enough leases! it means we are searching in the future for preloading!
+	//queryUpdate = strings.Replace(`UPDATE "$TOPIC" SET timeline = GREATEST($1, timeline) + $2, consumer_id = $3 WHERE
+	//	( timeline <= $1 OR (timeline > $1 AND timeline <= $4 AND consumer_id = NULL ))
+	//	AND bucket_id IN ($BUCKETS)
+	//	LIMIT $5
+	//	RETURNING id;
+	//`, "$TOPIC", table(string(timelineID)), -1)
+	//queryUpdate = strings.Replace(queryUpdate, "$BUCKETS", bucketsAsCSV, 1)
+	////Note: the server will automatically do some retries https://www.cockroachlabs.com/docs/stable/transactions.html#automatic-retries
+	//
+	//futureLeases, _, err := c.leaseByQuery(ctx, queryUpdate, consumerId, leaseMs, maxTimestamp, limit-len(availableLeases), timelineID)
+	//
+	//return append(availableLeases, futureLeases...), hasMore, err
+	return availableLeases, hasMore, err
+}
+
+func (c *CRClient) leaseByQuery(request leaseQuery) ([]timeline.Lease, bool, error) {
+	st, err := c.db.PrepareContext(request.ctx, request.queryUpdate)
 	if err != nil {
 		return nil, false, err
 	}
 
-	consumerStr := *(*string)(unsafe.Pointer(&consumerId))
+	consumerStr := *(*string)(unsafe.Pointer(&request.consumerId))
 	rows, err := st.Query(
-		dtime.TimeToMS(time.Now()), leaseMs, consumerStr,
-		maxTimestamp, limit,
+		request.maxTimestamp,                    //$1
+		request.leaseDurationMS,                 //$2
+		consumerStr,                             //$3
+		padBucketID(request.minInclusiveBucket), //$4
+		padBucketID(request.maxExclusiveBucket), //$5
+		request.limit,                           //$6
 		//TODO make the named parameter work :/ or file a bug report
 		//sql.Named("now", dtime.TimeToMS(time.Now())),
-		//sql.Named("lease", leaseMs),
+		//sql.Named("lease", leaseDurationMS),
 		//sql.Named("consumerid", []byte(consumerId)),
 		//sql.Named("maxts", maxTimestamp),
 		//sql.Named("limit", limit),
 	)
 	if err != nil {
-		return nil, false, c.externalErr(ctx, err, "getAndLease")
+		return nil, false, c.externalErr(request.ctx, err, "getAndLease")
 	}
 	defer rows.Close()
+
+	//TODO warning! if the method fails from here on or the broker dies, the messages will remain locked with a lease
 
 	// Print out the returned value.
 	msgIDs := []interface{}{}
@@ -209,7 +316,7 @@ func (c *CRClient) GetAndLease(ctx context.Context, timelineID []byte, buckets d
 	for rows.Next() {
 		var id string
 		if err := rows.Scan(&id); err != nil {
-			return nil, false, c.externalErr(ctx, err, "getAndLease")
+			return nil, false, c.externalErr(request.ctx, err, "getAndLease")
 		}
 		//https://www.cockroachlabs.com/docs/v19.2/bytes.html#main-content
 		msgIDs = append(msgIDs, id)
@@ -229,13 +336,13 @@ func (c *CRClient) GetAndLease(ctx context.Context, timelineID []byte, buckets d
 	//TODO Prepare does notwork in IN statements, do a manual escape for sql injections
 	querySelect := fmt.Sprintf(`SELECT 
 		topic.id, topic.timeline, topic.bucket_id, topic.ts, topic.producer_group_id, topic.version, topic.body
-		FROM "%s" AS topic WHERE topic.id IN (%s);`,
-		table(string(timelineID)), strings.Join(params, ","))
+		FROM "%s" AS topic WHERE topic.bucketid_msgid IN (%s);`,
+		table(string(request.timelineID)), strings.Join(params, ","))
 
-	sel, err := c.db.PrepareContext(ctx, querySelect)
+	sel, err := c.db.PrepareContext(request.ctx, querySelect)
 	if err != nil {
 		//TODO revert the leases
-		return nil, false, c.externalErr(ctx, err, "getAndLease")
+		return nil, false, c.externalErr(request.ctx, err, "getAndLease")
 	}
 	defer sel.Close()
 
@@ -258,21 +365,21 @@ func (c *CRClient) GetAndLease(ctx context.Context, timelineID []byte, buckets d
 			&id, &lease.ExpirationTimestampMS, &lease.Message.BucketID, &lease.Message.TimestampMS, &producerID, &lease.Message.Version, &body,
 		); err != nil {
 			//TODO revert the leases
-			return nil, true, c.externalErr(ctx, err, "getAndLease")
+			return nil, true, c.externalErr(request.ctx, err, "getAndLease")
 		}
 		lease.Message.ID = []byte(id)
 		lease.Message.ProducerGroupID = []byte(producerID)
-		lease.ConsumerID = consumerId
+		lease.ConsumerID = request.consumerId
 		lease.Message.Body = []byte(body)
 		result = append(result, lease)
 	}
 
 	if len(result) != len(msgIDs) {
 		//TODO revert the leases
-		return result, true, c.externalErr(ctx, err, "getAndLease")
+		return result, true, c.externalErr(request.ctx, err, "getAndLease")
 	}
 	//we do not know if there are more rows, just assume
-	return result, len(result) == limit, nil
+	return result, len(result) == request.limit, nil
 }
 
 func (c *CRClient) Delete(ctx context.Context, request timeline.DeleteMessagesRequest) error {
@@ -280,7 +387,7 @@ func (c *CRClient) Delete(ctx context.Context, request timeline.DeleteMessagesRe
 	result := make(derrors.MessageIDTupleList, 0)
 	ids := make([]string, len(request.Messages))
 	for i := range request.Messages {
-		ids[i] = request.Messages[i].GetMessageID()
+		ids[i] = timelineMsgID(request.Messages[i].BucketID, request.Messages[i].GetMessageID())
 	}
 
 	failBatch := func(err error) {
@@ -318,7 +425,7 @@ func (c *CRClient) Delete(ctx context.Context, request timeline.DeleteMessagesRe
 		//the same queries works for bodies as well
 
 		//TODO Prepare does not work in IN statements, do a manual escape for sql injections
-		queryPattern := fmt.Sprintf(`DELETE FROM "$TABLE" WHERE id IN (%s) LIMIT %d RETURNING NOTHING;`, strings.Join(params, ","), len(batch))
+		queryPattern := fmt.Sprintf(`DELETE FROM "$TABLE" WHERE bucketid_msgid IN (%s) LIMIT %d RETURNING NOTHING;`, strings.Join(params, ","), len(batch))
 
 		//args := make([]interface{}, len(batch))
 		//for i := range batch {
